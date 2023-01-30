763dcb281a6970a0216234e42d83cd9a6aa27ca4
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2023-01-31 01:18:37 +0900

    rm: no need lines

--- robot.py
+++ robot.py
@@ -206,19 +206,6 @@ class Gnss(IdealGnss):
         
 
 # %%
-
-world = World(30, 0.1, debug=False)     
-
-### ロボットを作る ###
-straight = Agent(0.2, 0.0)    
-circling = Agent(0.2, 10.0/180*math.pi)  
-r = Robot(np.array([2, 2, math.pi/6]).T, gnss=Gnss(0.1), agent=circling) 
-world.append(r)
-
-### アニメーション実行 ###
-world.draw()
-
-# %%
 if __name__ == '__main__': 
     world = World(30, 0.1, debug=False)     
 
